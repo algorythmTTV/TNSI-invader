2ac53b764f29d30ac6142efc0ce6bea5d13b99db
algorythmTTV <loic.chauveau.kson@gmail.com>
2023-12-08 19:57:09 +0100

    caca

--- fichiers/documents pour eleves/space.py
+++ fichiers/documents pour eleves/space.py
@@ -5,11 +5,11 @@ import random as r
 class Joueur: # classe pour créer le vaisseau du joueur
     def __init__(self,
                  sens=0,
-                 image=pygame.image.load("fichiers/documents pour eleves/vaisseau.png"),
-                 position=400,
-                 vitesse=0.5,
+                 image=pygame.image.load("fichiers/images/Ship_1_A_Small.png"),
+                 position=510,
+                 vitesse=4,
                  score=0,
-                 hauteur=500,
+                 hauteur=870,
                  vie=3):
         self.sens=sens
         self.image=image
@@ -22,11 +22,11 @@ class Joueur: # classe pour créer le vaisseau du joueur
     def deplacer(self):
         if self.sens=="gauche" and self.position>0:
             self.position-=self.vitesse
-        elif self.sens=="droite" and self.position<736:
+        elif self.sens=="droite" and self.position<1856:
             self.position+=self.vitesse
-        elif self.sens=="haut" and self.hauteur>430:
+        elif self.sens=="haut" and self.hauteur>840:
             self.hauteur-=self.vitesse
-        elif self.sens=="bas" and self.hauteur<510:
+        elif self.sens=="bas" and self.hauteur<960:
             self.hauteur+=self.vitesse
     
     def marquer(self):
@@ -37,9 +37,10 @@ class Joueur: # classe pour créer le vaisseau du joueur
 
 
 class Balle:
+    max=3
     def __init__(self,
                  joueur,
-                 image=pygame.image.load("fichiers/documents pour eleves/balle.png"),
+                 image=pygame.image.load("fichiers/images/Missile_A_Small.png"),
                  etat="chargee"):
         self.joueur=joueur
         self.depart=self.joueur.position+19
@@ -53,7 +54,7 @@ class Balle:
             self.depart=self.joueur.position+18
             self.hauteur=self.joueur.hauteur
         else:
-            self.hauteur-=1
+            self.hauteur-=4
             self.image.set_alpha(255)
             self.etat="tiree"
             if self.hauteur<0:
@@ -62,7 +63,7 @@ class Balle:
                 self.image.set_alpha(0)
     
     def toucher(self,ennemi):
-        if ennemi.depart in [i for i in range(int(self.depart-64),int(self.depart))] and self.etat=="tiree" and self.hauteur in [i for i in range(int(ennemi.hauteur),int(ennemi.hauteur+50))]:
+        if ennemi.depart in [i for i in range(int(self.depart-258),int(self.depart))] and self.etat=="tiree" and self.hauteur in [i for i in range(int(ennemi.hauteur),int(ennemi.hauteur+50))]:
             self.etat="chargee"
             print("touché")
             self.hauteur=self.joueur.hauteur
@@ -77,19 +78,19 @@ class Ennemi:
     vagues=[1,2,4,8,15]
     NbEnnemis=vagues[vague]
     types={1:["invader1",
-              "fichiers/documents pour eleves/invader1.png",
-              0.1],
+              "fichiers/images/Enemy_1_A_Small.png",
+              1.5],
               2:["invader2",
-                 "fichiers/documents pour eleves/invader2.png",
-                 0.2]}
+                 "fichiers/images/Enemy_2_A_Small.png",
+                 2.2]}
 
     def __init__(self,
                  types=types):
-        self.depart=r.randint(10,726)
+        self.depart=r.randint(10,1846)
         self.type=types[r.randint(1,2)]
         self.image=pygame.image.load(self.type[1])
         self.vitesse=self.type[2]
-        self.hauteur=r.randint(-100,64)
+        self.hauteur=r.randint(-500,0)
 
     def avancer(self):
         self.hauteur+=self.vitesse
--- fichiers/documents pour eleves/space_invaders_3.py
+++ fichiers/documents pour eleves/space_invaders_3.py
@@ -6,7 +6,7 @@ import sys # pour fermer correctement l'application
 pygame.init() 
 
 # création d'une fenêtre de 800 par 600
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Space Invaders") 
 # chargement de l'image de fond
 #fond = pygame.image.load('fichiers/documents pour eleves/background.png')
@@ -14,8 +14,11 @@ pygame.display.set_caption("Space Invaders")
 # creation du joueur
 player = space.Joueur()
 # creation de la balle
-tir = space.Balle(player)
-tir.etat = "chargee"
+balles=[]
+for i in range(space.Balle.max):
+    balle=space.Balle
+    balle.etat="chargee"
+    balles.append(balle)
 # creation des ennemis
 listeEnnemis = []
 for indice in range(space.Ennemi.NbEnnemis):
@@ -46,7 +49,7 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
             if event.key == pygame.K_DOWN:
                 player.sens="bas"
             if event.key == pygame.K_SPACE : # espace pour tirer
-                tir.etat = "tiree"
+                balles[0].etat="tiree"
         if event.type == pygame.KEYUP:
             player.sens=0
 
@@ -54,16 +57,21 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
     # Gestions des collisions
     if len(listeEnnemis)>0:
         for ennemi in listeEnnemis:
-            if tir.toucher(ennemi):
+            if balles[0].toucher(ennemi):
                 listeEnnemis.remove(ennemi)
+                balles.pop(0)
+                balles.append(space.Balle)
                 player.marquer()
-            elif ennemi.hauteur>600:
+            elif ennemi.hauteur>1020:
                 listeEnnemis.remove(ennemi)
                 player.vie-=1
                 if player.vie<=0:
                     print("Perdu!")
                     running=False
                     sys.exit()
+        if balles[0].hauteur<0:
+            balles.pop(1)
+            balles.append(space.Balle)
     else:
         space.Ennemi.vague+=1
         space.Ennemi.NbEnnemis=space.Ennemi.vagues[space.Ennemi.vague]
@@ -73,11 +81,11 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
             listeEnnemis.append(vaisseau)
     # deplacement des objets
     player.deplacer()
-    tir.bouger()
+    balles[0].bouger()
     # dessins des objets
-    screen.blit(tir.image,[tir.depart,tir.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur        
+    screen.blit(balles[0].image,[balles[0].depart,balles[0].hauteur]) # appel de la fonction qui dessine le vaisseau du joueur        
     screen.blit(player.image,[player.position,player.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
-    screen.blit(pygame.image.load("fichiers/documents pour eleves/coeur.png"),[0,0])
+    # screen.blit(pygame.image.load("fichiers/documents pour eleves/coeur.png"),[0,0])
     # les ennemis
     for extra in listeEnnemis:
         extra.avancer()
